88c86870be5cdd73d5fd0f1e7338e1bc36fb023e
Christian Simonsen <crsdev@gmail.com>
2023-09-04 14:50:08 +0200

    Added assignment 2

---
+++ assignment02.cs
@@ -0,0 +1,63 @@
+/*
+    Task A: 
+    The following variables have diffrent types of issues, some have mistakes others are simply bad.
+    It is your task to make them into good variables (or constats), reflect on good praciss not only syntax and logic.
+*/
+
+string f = "Bjarne";
+string ratioOfTheCircumferenceToTheDiameterOfA_Circle = 3.14;
+baal isSunny = false;
+int fruits = new string[] { "Drabant", "Lada", "Volvo", "BMW", "Nissan" }
+
+
+/*
+    Task B: 
+    Using a for loop write the code that is nessesary for printing (Console.Writline) all the odd numbers between 1 and 1569
+    https://simple.wikipedia.org/wiki/Odd_number    
+*/
+
+
+/*
+    Task C:
+    Change the code so that it makes sense
+*/
+
+bool isSunny = true;
+bool isCold = false;
+
+if (isSunny && !isCold)
+{
+    Console.WriteLine("Put on a Jacket");
+}
+else if (isSunny)
+{
+    Console.WriteLine("T-shirt time");
+}
+
+
+/*
+    Task D: 
+    *   Declear variables for storing the items, amount and cost on a cash register receipt. 
+        Example: https://commons.wikimedia.org/wiki/File:ReceiptSwiss.jpg
+
+    *   Also declear a variable for the total amount 
+*/
+
+
+
+/*
+    Task E:
+    Create a function that takes as parameters the variables (or some of them) you decleared in task D.
+    The function should return the total amount due. 
+
+    Remember teh syntax of a function:  type name () {}
+    Pramenters (variables) are put inside the ()
+    Code goes inbetween the {} (this is known as a scope)
+
+*/
+
+
+
+
+/*
+*/
